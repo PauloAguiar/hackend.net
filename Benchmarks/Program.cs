01541acce91b8ff00e6f2add4e61fc3292d772c7
Paulo Aguiar <1757521+PauloAguiar@users.noreply.github.com>
2024-02-24 01:58:26 -0800

    Fix missing files

--- Benchmarks/Program.cs
+++ Benchmarks/Program.cs
@@ -13,14 +13,22 @@ public class ApiCallBenchmark
     private readonly Uri requestUri;
     private readonly Uri requestUri2;
     private readonly Uri requestUri3;
+    private readonly Uri requestUri4;
+    private readonly Uri requestUri5;
+    private readonly Uri requestUri6;
 
     public ApiCallBenchmark()
     {
         client = new HttpClient();
 
-        this.requestUri = new Uri("http://nginx:10002/clientes/4/transacoes");
-        this.requestUri2 = new Uri("http://ph-nginx:10000/clientes/4/transacoes");
-        this.requestUri3 = new Uri("http://tomer-nginx:9999/clientes/4/transacoes");
+        this.requestUri = new Uri("http://nginx:10002/clientes/4/extrato");
+        this.requestUri2 = new Uri("http://ph-nginx:10000/clientes/4/extrato");
+        this.requestUri3 = new Uri("http://tomer-nginx:9999/clientes/4/extrato");
+        
+        this.requestUri4 = new Uri("http://nginx:10002/clientes/4/transacoes");
+        this.requestUri5 = new Uri("http://ph-nginx:10000/clientes/4/transacoes");
+        this.requestUri6 = new Uri("http://tomer-nginx:9999/clientes/4/transacoes");
+
         string jsonContent = "{\"valor\":1,\"tipo\":\"d\",\"descricao\":\"devolve\"\r\n}";
         content = new StringContent(jsonContent, Encoding.UTF8,  "application/json");
     }
@@ -41,24 +49,42 @@ public class ApiCallBenchmark
         //await client.GetAsync("http://ph-nginx:10000/wipe");
     }
 
-    //[Benchmark]
-    public async Task Goku()
+    [Benchmark]
+    public async Task GokuExtrato()
     {
         // var response = await client.PostAsync(requestUri, content);
         var response = await client.GetAsync(requestUri);
     }
 
     [Benchmark]
-    public async Task Doente()
+    public async Task DoenteExtrato()
     {
-        var response = await client.PostAsync(requestUri2, content);
-        // var response = await client.GetAsync(requestUri2);
+        // var response = await client.PostAsync(requestUri2, content);
+        var response = await client.GetAsync(requestUri2);
     }
 
-    //[Benchmark]
-    public async Task Tomer()
+    [Benchmark]
+    public async Task TomerExtrato()
     {
         // var response = await client.PostAsync(requestUri3, content);
         var response = await client.GetAsync(requestUri3);
     }
+    [Benchmark]
+    public async Task GokuTransacoes()
+    {
+        var response = await client.PostAsync(requestUri4, content);
+    }
+
+    [Benchmark]
+    public async Task DoenteTransacoes()
+    {
+        var response = await client.PostAsync(requestUri5, content);
+    }
+
+    [Benchmark]
+    public async Task TomerTransacoes()
+    {
+        var response = await client.PostAsync(requestUri6, content);
+    }
+
 }
